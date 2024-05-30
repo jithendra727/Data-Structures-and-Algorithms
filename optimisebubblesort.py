99210ad9abde42ee9ef916a8d3c8042542d44f98
Jithendra Janakantha <79997272+jithendra727@users.noreply.github.com>
2024-05-30 14:59:14 +0530

    Add files via upload

---
+++ optimisebubblesort.py
@@ -0,0 +1,14 @@
+def optimisedbubblesort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range (0, n - i - 1):
+            if arr[j]> arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+arr = [74, 92, 16, 37, 28, 48, 94, 12]
+optimisedbubblesort(arr)
+print("Sorted array is : ", arr)
